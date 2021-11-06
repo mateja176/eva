6205a8152856ead70355eb3e57abe958144efca9
Mateja <mateja176@gmail.com>
2021-11-06 12:19:16 +0100

    implements proof of concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useFormik } from 'formik';
 import './App.css';
 
+const reactAppApi = process.env.REACT_APP_API;
+if (!reactAppApi) {
+  throw new Error('Please provide REACT_APP_API');
+}
+
+const initialUser = {
+  firstName: '',
+  lastName: '',
+  age: 0,
+  email: '',
+};
+type User = typeof initialUser;
+
+const onSubmitUser = (user: User): Promise<{ id: string }> => {
+  return fetch(reactAppApi, {
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify(user),
+  }).then((res) => res.json());
+};
+
 function App() {
+  const { handleSubmit, getFieldProps } = useFormik({
+    initialValues: initialUser,
+    onSubmit: onSubmitUser,
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          First Name
+          <input type="text" {...getFieldProps('firstName')} />
+        </label>
+        <label>
+          Last Name
+          <input type="text" {...getFieldProps('lastName')} />
+        </label>
+        <label>
+          Age
+          <input type="number" {...getFieldProps('age')} />
+        </label>
+        <label>
+          Email
+          <input type="email" {...getFieldProps('email')} />
+        </label>
+        <input type="submit" />
+      </form>
     </div>
   );
 }
